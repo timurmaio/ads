3465469911d4b6980554d4221bcb677956eb9bf0
Timur Platonov <tpltnz@gmail.com>
2018-02-01 10:15:19 +0300

    add reverseInput

---
+++ src/test/algorithms/ReverseInputTest.kt
@@ -0,0 +1,23 @@
+package test.algorithms
+
+import algorithms.reverseInput
+import org.testng.annotations.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import kotlin.test.assertEquals
+
+class ReverseInputTest {
+    @Test
+    fun extendedEuclideanTest() {
+        // stub IO
+        val input = "1\n2\n3"
+        val output = ByteArrayOutputStream()
+        val outStream = PrintStream(output)
+        System.setIn(input.byteInputStream())
+        System.setOut(outStream)
+
+        reverseInput()
+
+        assertEquals("3 2 1 ", output.toString())
+    }
+}
