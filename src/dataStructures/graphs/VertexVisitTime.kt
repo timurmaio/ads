6143efe161124db36835fe03679a94a62ce76c90
Timur Platonov <tpltnz@gmail.com>
2018-02-04 12:26:04 +0300

    add graph

--- src/dataStructures/graphs/VertexVisitTime.kt
+++ src/dataStructures/graphs/VertexVisitTime.kt
@@ -3,4 +3,4 @@ package dataStructures.graphs
 /**
  * Представление пары моментов времени начала и окончания обработки вершины при обходе графа.
  */
-data class VertexVisitTime(var entry: Int, val leave: Int)
+data class VertexVisitTime(var entry: Int, var leave: Int)
